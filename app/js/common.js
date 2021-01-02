c6ea0726707e0872b687c491e98a0187d865d1e1
Viktoria <zaharova181114@gmail.com>
2021-01-03 00:00:24 +0200

    add modal

--- app/js/common.js
+++ app/js/common.js
@@ -44,3 +44,45 @@ $('.brands-slider').slick({
 		'</g>\n' +
 		'</svg>\n</button>',
 });
+
+// модальные окна (несколько)
+$(function () {
+	var overlay = $('.overlay');
+	var open_modal = $('.open_modal');
+	var close = $('.modal__close, .overlay');
+	var modal = $('.modal__div');
+
+	open_modal.on('click',function (event) {
+		event.preventDefault();
+
+		modal.css('display', 'none').animate({
+			opacity: 0,
+			top: '45%'
+		}, 200);
+
+		var div = $(this).attr('href');
+		overlay.fadeIn(400,
+			function () {
+				$(div)
+					.css('display', 'flex')
+					.animate({
+						opacity: 1,
+						top: '50%'
+					}, 200);
+			});
+	});
+
+	close.on('click', function () {
+		modal
+			.animate({
+				opacity: 0,
+				top: '45%'
+			}, 200,
+				function () {
+					$(this).css('display', 'none');
+					overlay.fadeOut(400);
+				}
+			);
+	});
+});
+//end
