a87a9d03b325c7c2610c30286fd4f08185bde2c8
Viktoria <zaharova181114@gmail.com>
2021-01-08 18:56:00 +0200

    edit css + js

--- app/js/common.js
+++ app/js/common.js
@@ -301,3 +301,17 @@ $('.load-more').on('click', function (e) {
 		$('.load-more').hide();
 	}
 });
+
+// stars rating
+$('.list-estimate li').on('click', function () {
+	var onStar = parseInt($(this).data('value'), 10); // The star currently selected
+	var stars = $(this).parent().children('li.star');
+
+	for (i = 0; i < stars.length; i++) {
+		$(stars[i]).removeClass('selected');
+	}
+
+	for (i = 0; i < onStar; i++) {
+		$(stars[i]).addClass('selected');
+	}
+});
